6ab0446316e5c4dd14afd05974970ce73860840c
Amiga501 <brendan.sloan@live.co.uk>
2025-12-01 21:07:10 +0000

    Update panel_main.py

--- py_contractor/dash/panel_main.py
+++ py_contractor/dash/panel_main.py
@@ -14,11 +14,14 @@ Basic plan is to have 3x panels for initial MVP:
 """
 # %% Global imports
 from collections.abc import Callable
+from dotenv import load_dotenv
 
 import panel as pn
+import os
 
 
 # %% py_contractor imports
+from py_contractor.config.config import Config
 from py_contractor.config.loggers import DashLogger
 
 
@@ -28,10 +31,11 @@ pn.extension("tabulator")
 
 LOGGER = DashLogger().logger
 
+load_dotenv(Config().ENV_DIR)
 
 # %% Functions
 
-def create_app():
+def main_app():
     """!
     **Create the app**
     
@@ -40,7 +44,7 @@ def create_app():
         logger=LOGGER,
         )
     
-    return app
+    return app.run()
 
 
 # %% Classes
@@ -62,7 +66,18 @@ class PanelApp:
         """        
         self.logger = logger
         
+        if not self.__get_env_vars():
+            self.template = None
+            return
+        
         self.__create_template()
+        
+    # -------------------------------------------------------------------------
+    def __create_sidebar(self):
+        """!
+        **Create the sidebar links**
+        
+        """
         
     # -------------------------------------------------------------------------
     def __create_template(self):
@@ -82,6 +97,12 @@ class PanelApp:
             )
         
         self.template.main = self.tabs
+        
+    # -------------------------------------------------------------------------
+    def __create_template_authorisation(self):
+        """!
+        **Create the authorisation template**
+        """
         
     # -------------------------------------------------------------------------
     def __create_template_summary(self):
@@ -110,3 +131,49 @@ class PanelApp:
             )
         
     # -------------------------------------------------------------------------
+    def __get_env_vars(self) -> bool:
+        """!
+        **Pull env vars into the class**
+        
+        @return [bool] True if all necessary vars found, False otherwise
+        
+        """        
+        vars_ = ["CLIENT_SECRET",
+                 "CLIENT_ID",
+                 "REDIRECT_URI",
+                 "SCOPE",
+                 "OAUTH_AUTHORISE_URL",
+                 "OAUTH_TOKEN_URL"
+                 ]
+        
+        for var_ in vars_:
+            if not (val := os.environ.get(var_)):
+                self.logger.error(
+                    f"Required environmental var: {var_} not found, won't be "
+                    "able to complete setup")
+                return False
+            setattr(self, var_.lower(), val)
+        
+        return True
+    
+    # -------------------------------------------------------------------------
+    def run(self):
+        """!
+        **Run the webapp**
+        """
+        return self.template
+            
+        
+    
+    
+# %% Main
+if __name__ == "__main__":
+    
+    pn.serve(
+        main_app,
+        port=5006,
+        threaded=True,
+        )
+
+else:
+    main_app().servable()
